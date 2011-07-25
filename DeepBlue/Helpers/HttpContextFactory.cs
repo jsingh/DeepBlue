e7ae2b0b562c0575143f8764635ab9a743b8f036
unknown <Administrator@.(none)>
2011-07-25 19:36:09 +0530

    missed Files

---
+++ DeepBlue/Helpers/HttpContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeepBlue.Helpers {
+    public static class HttpContextFactory {
+        [ThreadStatic]
+        private static HttpContextBase mockHttpContext;
+
+        public static void SetHttpContext(HttpContextBase httpContextBase) {
+            mockHttpContext = httpContextBase;
+        }
+
+        public static void ResetHttpContext() {
+            mockHttpContext = null;
+        }
+
+        public static HttpContextBase GetHttpContext() {
+            if (mockHttpContext != null) {
+                return mockHttpContext;
+            }
+
+            if (HttpContext.Current != null) {
+                return new HttpContextWrapper(HttpContext.Current);
+            }
+
+            return null;
+        }
+    }
+}
