dc45f27f489c56959e57f9a6a89c1a353433e2a6
unknown <Administrator@.(none)>
2011-08-26 18:45:08 +0530

    Login Fix Issue

--- DeepBlue/Controllers/Account/AccountRepository.cs
+++ DeepBlue/Controllers/Account/AccountRepository.cs
@@ -15,13 +15,13 @@ namespace DeepBlue.Controllers.Account {
 
 		public ENTITY FetchUserEntity(int entityId) {
 			using (DeepBlueEntities context = new DeepBlueEntities()) {
-				return context.ENTITies.Where(entity => entity.EntityID == entityId).SingleOrDefault();
+				return context.ENTITies.Where(entity => entity.EntityID == entityId && entity.Enabled == true).SingleOrDefault();
 			}
 		}
 
 		public USER FetchUserLogin(string userName, int entityId) {
 			using (DeepBlueEntities context = new DeepBlueEntities()) {
-				return context.USERs.Where(user => user.Login == userName && user.EntityID == entityId).SingleOrDefault();
+				return context.USERs.Where(user => user.Login == userName && user.EntityID == entityId && user.Enabled == true).SingleOrDefault();
 			}
 		}
 	}
